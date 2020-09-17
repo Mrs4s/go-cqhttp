ecd239bc7080aec2547716071d4756d043a30c8f
scjtqs <scjtqs@qq.com>
2020-09-17 14:06:15 +0800

    测试

--- web/web.go
+++ web/web.go
@@ -1,9 +1,12 @@
 package web
 
 import (
+	"bufio"
+	"bytes"
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
+	"github.com/Mrs4s/MiraiGo/client"
 	"github.com/Mrs4s/go-cqhttp/coolq"
 	"github.com/Mrs4s/go-cqhttp/global"
 	"github.com/gin-gonic/gin"
@@ -11,7 +14,10 @@ import (
 	"github.com/google/uuid"
 	log "github.com/sirupsen/logrus"
 	"github.com/tidwall/gjson"
+	asciiart "github.com/yinghau76/go-ascii-art"
 	"html/template"
+	"image"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
@@ -27,10 +33,9 @@ type webServer struct {
 
 var WebServer = &webServer{}
 
-func (s *webServer) Run(addr string, bot *coolq.CQBot) {
+func (s *webServer) Run(addr string, cli *client.QQClient)  *coolq.CQBot{
 	gin.SetMode(gin.ReleaseMode)
 	s.engine = gin.New()
-	s.bot = bot //外部引入 bot对象，用于操作bot
 	// 自动加载模板
 	t := template.New("")
 	//func 函数映射 全局模板可用
@@ -63,26 +68,26 @@ func (s *webServer) Run(addr string, bot *coolq.CQBot) {
 		if c.Request.Method != "GET" && c.Request.Method != "POST" {
 			log.Warnf("已拒绝客户端 %v 的请求: 方法错误", c.Request.RemoteAddr)
 			c.Status(404)
-			return
+			c.Abort()
 		}
 		if c.Request.Method == "POST" && strings.Contains(c.Request.Header.Get("Content-Type"), "application/json") {
 			d, err := c.GetRawData()
 			if err != nil {
 				log.Warnf("获取请求 %v 的Body时出现错误: %v", c.Request.RequestURI, err)
 				c.Status(400)
-				return
+				c.Abort()
 			}
 			if !gjson.ValidBytes(d) {
 				log.Warnf("已拒绝客户端 %v 的请求: 非法Json", c.Request.RemoteAddr)
 				c.Status(400)
-				return
+				c.Abort()
 			}
 			c.Set("json_body", gjson.ParseBytes(d))
 		}
 		c.Next()
 	})
 	go func() {
-		log.Infof("CQ HTTP 服务器已启动: %v", addr)
+		log.Infof("miraigo webui 服务器已启动: %v", addr)
 		err := s.engine.Run(addr)
 		if err != nil {
 			log.Error(err)
@@ -91,6 +96,112 @@ func (s *webServer) Run(addr string, bot *coolq.CQBot) {
 			os.Exit(1)
 		}
 	}()
+	go s.Dologin(cli)
+	b:=s.bot //外部引入 bot对象，用于操作bot
+	return b
+}
+
+func (s *webServer) Dologin(cli *client.QQClient)  {
+	console := bufio.NewReader(os.Stdin)
+	conf:=GetConf()
+	rsp, err := cli.Login()
+	for {
+		global.Check(err)
+		if !rsp.Success {
+			switch rsp.Error {
+			case client.NeedCaptcha:
+				_ = ioutil.WriteFile("captcha.jpg", rsp.CaptchaImage, 0644)
+				img, _, _ := image.Decode(bytes.NewReader(rsp.CaptchaImage))
+				fmt.Println(asciiart.New("image", img).Art)
+				log.Warn("请输入验证码 (captcha.jpg)： (Enter 提交)")
+				text, _ := console.ReadString('\n')
+				rsp, err = cli.SubmitCaptcha(strings.ReplaceAll(text, "\n", ""), rsp.CaptchaSign)
+				continue
+			case client.UnsafeDeviceError:
+				log.Warnf("账号已开启设备锁，请前往 -> %v <- 验证并重启Bot.", rsp.VerifyUrl)
+				log.Infof(" 按 Enter 继续....")
+				_, _ = console.ReadString('\n')
+				return
+			case client.OtherLoginError, client.UnknownLoginError:
+				log.Fatalf("登录失败: %v", rsp.ErrorMessage)
+				return
+			}
+		}
+		break
+	}
+	log.Infof("登录成功 欢迎使用: %v", cli.Nickname)
+	time.Sleep(time.Second)
+	log.Info("开始加载好友列表...")
+	global.Check(cli.ReloadFriendList())
+	log.Infof("共加载 %v 个好友.", len(cli.FriendList))
+	log.Infof("开始加载群列表...")
+	global.Check(cli.ReloadGroupList())
+	log.Infof("共加载 %v 个群.", len(cli.GroupList))
+	s.bot= coolq.NewQQBot(cli, conf)
+	if conf.PostMessageFormat != "string" && conf.PostMessageFormat != "array" {
+		log.Warnf("post_message_format 配置错误, 将自动使用 string")
+		coolq.SetMessageFormat("string")
+		return
+	} else {
+		coolq.SetMessageFormat(conf.PostMessageFormat)
+	}
+	if conf.RateLimit.Enabled {
+		global.InitLimiter(conf.RateLimit.Frequency, conf.RateLimit.BucketSize)
+	}
+	log.Info("正在加载事件过滤器.")
+	global.BootFilter()
+	coolq.IgnoreInvalidCQCode = conf.IgnoreInvalidCQCode
+	coolq.ForceFragmented = conf.ForceFragmented
+	//if conf.HttpConfig != nil && conf.HttpConfig.Enabled {
+	//	go server.HttpServer.Run(fmt.Sprintf("%s:%d", conf.HttpConfig.Host, conf.HttpConfig.Port), conf.AccessToken, s.bot)
+	//	for k, v := range conf.HttpConfig.PostUrls {
+	//		server.NewHttpClient().Run(k, v, conf.HttpConfig.Timeout, s.bot)
+	//	}
+	//}
+	//if conf.WSConfig != nil && conf.WSConfig.Enabled {
+	//	go server.WebsocketServer.Run(fmt.Sprintf("%s:%d", conf.WSConfig.Host, conf.WSConfig.Port), conf.AccessToken, s.bot)
+	//}
+	//for _, rc := range conf.ReverseServers {
+	//	go server.NewWebsocketClient(rc, conf.AccessToken, s.bot).Run()
+	//}
+	log.Info("资源初始化完成, 开始处理信息.")
+	log.Info("アトリは、高性能ですから!")
+	cli.OnDisconnected(func(bot *client.QQClient, e *client.ClientDisconnectedEvent) {
+		if conf.ReLogin.Enabled {
+			var times uint = 1
+			for {
+				if conf.ReLogin.MaxReloginTimes == 0 {
+				} else if times > conf.ReLogin.MaxReloginTimes {
+					break
+				}
+				log.Warnf("Bot已离线 (%v)，将在 %v 秒后尝试重连. 重连次数：%v",
+					e.Message, conf.ReLogin.ReLoginDelay, times)
+				times++
+				time.Sleep(time.Second * time.Duration(conf.ReLogin.ReLoginDelay))
+				rsp, err := cli.Login()
+				if err != nil {
+					log.Errorf("重连失败: %v", err)
+					continue
+				}
+				if !rsp.Success {
+					switch rsp.Error {
+					case client.NeedCaptcha:
+						log.Fatalf("重连失败: 需要验证码. (验证码处理正在开发中)")
+					case client.UnsafeDeviceError:
+						log.Fatalf("重连失败: 设备锁")
+					default:
+						log.Errorf("重连失败: %v", rsp.ErrorMessage)
+						continue
+					}
+				}
+				log.Info("重连成功")
+				return
+			}
+			log.Fatal("重连失败: 重连次数达到设置的上限值")
+		}
+		s.bot.Release()
+		log.Fatalf("Bot已离线：%v", e.Message)
+	})
 }
 
 func (s *webServer) admin(c *gin.Context) {
@@ -246,6 +357,8 @@ func loadTemplate(t *template.Template) (*template.Template, error) {
 			return nil, err
 		}
 		//拼接方式，组装模板  admin/index.html 这种，方便调用
+		println(file)
+		println(strings.Replace(file, "html/", "", 1))
 		t, err = t.New(strings.Replace(file, "html/", "", 1)).Parse(h)
 		if err != nil {
 			return nil, err
