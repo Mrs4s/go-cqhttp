2d020bc7f72ff3ee95e827e641a9dde1cb86a164
wdvxdr <wdvxdr@foxmail.com>
2021-01-08 12:14:09 +0800

    drop  go-silk

--- global/codec.go
+++ global/codec.go
@@ -4,31 +4,25 @@ import (
 	"crypto/md5"
 	"errors"
 	"fmt"
+	"github.com/Mrs4s/go-cqhttp/global/codec"
+	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"path"
-	"sync"
-
-	log "github.com/sirupsen/logrus"
-	"github.com/wdvxdr1123/go-silk/silk"
 )
 
-var codec silk.Encoder
-var useCodec = true
-var once sync.Once
+var useSilkCodec = true
 
 func InitCodec() {
-	once.Do(func() {
-		log.Info("正在加载silk编码器...")
-		err := codec.Init("data/cache", "codec")
-		if err != nil {
-			log.Error(err)
-			useCodec = false
-		}
-	})
+	log.Info("正在加载silk编码器...")
+	err := codec.Init("data/cache", "codec")
+	if err != nil {
+		log.Error(err)
+		useSilkCodec = false
+	}
 }
 
 func Encoder(data []byte) ([]byte, error) {
-	if useCodec == false {
+	if useSilkCodec == false {
 		return nil, errors.New("no silk encoder")
 	}
 	h := md5.New()
--- go.mod
+++ go.mod
@@ -20,7 +20,6 @@ require (
 	github.com/syndtr/goleveldb v1.0.0
 	github.com/t-tomalak/logrus-easy-formatter v0.0.0-20190827215021-c074f06c5816
 	github.com/tidwall/gjson v1.6.7
-	github.com/wdvxdr1123/go-silk v0.0.0-20201210140933-bcdbcb2f1093
 	github.com/yinghau76/go-ascii-art v0.0.0-20190517192627-e7f465a30189
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324
