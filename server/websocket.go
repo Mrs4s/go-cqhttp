8d0a0b7f4d0e4e513d6d3f3a1117be6181d9676c
Mrs4s <1844812067@qq.com>
2021-02-06 19:11:24 +0800

    fix typo.

--- server/websocket.go
+++ server/websocket.go
@@ -601,7 +601,7 @@ var wsAPI = map[string]func(*coolq.CQBot, gjson.Result) coolq.MSG{
 	"get_essence_msg_list": func(bot *coolq.CQBot, p gjson.Result) coolq.MSG {
 		return bot.CQGetEssenceMessageList(p.Get("group_id").Int())
 	},
-	"check_urk_safely": func(bot *coolq.CQBot, p gjson.Result) coolq.MSG {
+	"check_url_safely": func(bot *coolq.CQBot, p gjson.Result) coolq.MSG {
 		return bot.CQCheckUrlSafely(p.Get("url").String())
 	},
 	"set_group_anonymous_ban": func(bot *coolq.CQBot, p gjson.Result) coolq.MSG {
