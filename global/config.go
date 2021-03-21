c693aa3548e5fa7ce5f313857798512e539742d9
scjtqs <scjtqs@qq.com>
2021-03-21 12:39:54 +0800

    go lint处理

--- global/config.go
+++ global/config.go
@@ -15,6 +15,7 @@ var json = jsoniter.ConfigCompatibleWithStandardLibrary
 // var currentPath = getCurrentPath()
 
 // var DefaultConfFile = path.Join(currentPath, "config.hjson")
+// DefaultConfFile 默认的配置文件
 var DefaultConfFile string = "config.hjson"
 
 // DefaultConfigWithComments 为go-cqhttp的默认配置文件
