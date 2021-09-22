f629eee7f22e1c65643cf82d7ce18c1537108c63
Mrs4s <mrs4sxiaoshi@gmail.com>
2021-09-22 16:48:59 +0800

    fix typo.

--- db/multidb.go
+++ db/multidb.go
@@ -2,24 +2,24 @@ package db
 
 import "github.com/pkg/errors"
 
-// MultiDatabase 多数据库支持
+// MultiDBImpl 多数据库支持
 // 写入会对所有 Backend 进行写入
 // 读取只会读取第一个库
-type MultiDatabase struct {
+type MultiDBImpl struct {
 	backends []IDatabase
 }
 
-func NewMultiDatabase(backends ...IDatabase) *MultiDatabase {
-	return &MultiDatabase{
+func NewMultiDatabase(backends ...IDatabase) *MultiDBImpl {
+	return &MultiDBImpl{
 		backends: backends,
 	}
 }
 
-func (db *MultiDatabase) UseDB(backend IDatabase) {
+func (db *MultiDBImpl) UseDB(backend IDatabase) {
 	db.backends = append(db.backends, backend)
 }
 
-func (db *MultiDatabase) Open() error {
+func (db *MultiDBImpl) Open() error {
 	for _, b := range db.backends {
 		if err := b.Open(); err != nil {
 			return errors.Wrap(err, "open backend error")
@@ -28,28 +28,28 @@ func (db *MultiDatabase) Open() error {
 	return nil
 }
 
-func (db *MultiDatabase) GetMessageByGlobalID(id int32) (IStoredMessage, error) {
+func (db *MultiDBImpl) GetMessageByGlobalID(id int32) (IStoredMessage, error) {
 	if len(db.backends) == 0 {
 		return nil, errors.New("database disabled")
 	}
 	return db.backends[0].GetMessageByGlobalID(id)
 }
 
-func (db *MultiDatabase) GetGroupMessageByGlobalID(id int32) (*StoredGroupMessage, error) {
+func (db *MultiDBImpl) GetGroupMessageByGlobalID(id int32) (*StoredGroupMessage, error) {
 	if len(db.backends) == 0 {
 		return nil, errors.New("database disabled")
 	}
 	return db.backends[0].GetGroupMessageByGlobalID(id)
 }
 
-func (db *MultiDatabase) GetPrivateMessageByGlobalID(id int32) (*StoredPrivateMessage, error) {
+func (db *MultiDBImpl) GetPrivateMessageByGlobalID(id int32) (*StoredPrivateMessage, error) {
 	if len(db.backends) == 0 {
 		return nil, errors.New("database disabled")
 	}
 	return db.backends[0].GetPrivateMessageByGlobalID(id)
 }
 
-func (db *MultiDatabase) InsertGroupMessage(m *StoredGroupMessage) error {
+func (db *MultiDBImpl) InsertGroupMessage(m *StoredGroupMessage) error {
 	for _, b := range db.backends {
 		if err := b.InsertGroupMessage(m); err != nil {
 			return errors.Wrap(err, "insert message to backend error")
@@ -58,7 +58,7 @@ func (db *MultiDatabase) InsertGroupMessage(m *StoredGroupMessage) error {
 	return nil
 }
 
-func (db *MultiDatabase) InsertPrivateMessage(m *StoredPrivateMessage) error {
+func (db *MultiDBImpl) InsertPrivateMessage(m *StoredPrivateMessage) error {
 	for _, b := range db.backends {
 		if err := b.InsertPrivateMessage(m); err != nil {
 			return errors.Wrap(err, "insert message to backend error")
