58a17c65a1c5976b79c75916e5557e0aaaad6f3e
风之凌殇 <fzls.zju@gmail.com>
2021-10-04 07:53:09 +0800

    feat: add console log color support

--- global/log_hook.go
+++ global/log_hook.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/gookit/color"
 	"github.com/sirupsen/logrus"
 )
 
@@ -183,6 +184,9 @@ type LogFormat struct{}
 func (f LogFormat) Format(entry *logrus.Entry) ([]byte, error) {
 	buf := NewBuffer()
 	defer PutBuffer(buf)
+
+	buf.WriteString(GetLogLevelColorCode(entry.Level))
+
 	buf.WriteByte('[')
 	buf.WriteString(entry.Time.Format("2006-01-02 15:04:05"))
 	buf.WriteString("] [")
@@ -190,6 +194,42 @@ func (f LogFormat) Format(entry *logrus.Entry) ([]byte, error) {
 	buf.WriteString("]: ")
 	buf.WriteString(entry.Message)
 	buf.WriteString(" \n")
+
+	buf.WriteString(color.ResetSet)
+
 	ret := append([]byte(nil), buf.Bytes()...) // copy buffer
 	return ret, nil
+}
+
+var (
+	colorCodePanic = fmt.Sprintf(color.SettingTpl, color.Style{color.Bold, color.Red}.String())
+	colorCodeFatal = fmt.Sprintf(color.SettingTpl, color.Style{color.Bold, color.Red}.String())
+	colorCodeError = fmt.Sprintf(color.SettingTpl, color.Style{color.Red}.String())
+	colorCodeWarn  = fmt.Sprintf(color.SettingTpl, color.Style{color.Yellow}.String())
+	colorCodeInfo  = fmt.Sprintf(color.SettingTpl, color.Style{color.Green}.String())
+	colorCodeDebug = fmt.Sprintf(color.SettingTpl, color.Style{color.White}.String())
+	colorCodeTrace = fmt.Sprintf(color.SettingTpl, color.Style{color.Cyan}.String())
+)
+
+// GetLogLevelColorCode 获取日志等级对应色彩code
+func GetLogLevelColorCode(level logrus.Level) string {
+	switch level {
+	case logrus.PanicLevel:
+		return colorCodePanic
+	case logrus.FatalLevel:
+		return colorCodeFatal
+	case logrus.ErrorLevel:
+		return colorCodeError
+	case logrus.WarnLevel:
+		return colorCodeWarn
+	case logrus.InfoLevel:
+		return colorCodeInfo
+	case logrus.DebugLevel:
+		return colorCodeDebug
+	case logrus.TraceLevel:
+		return colorCodeTrace
+
+	default:
+		return colorCodeInfo
+	}
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/dustin/go-humanize v1.0.0
 	github.com/fumiama/go-hide-param v0.1.4
 	github.com/gabriel-vasile/mimetype v1.3.1
+	github.com/gookit/color v1.4.2
 	github.com/gorilla/websocket v1.4.2
 	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
@@ -44,6 +45,7 @@ require (
 	github.com/tidwall/match v1.0.3 // indirect
 	github.com/tidwall/pretty v1.1.0 // indirect
 	github.com/willf/bitset v1.2.0 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	golang.org/x/net v0.0.0-20210505024714-0287a6fb4125 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
