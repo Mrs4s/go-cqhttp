67f0ea914d2650cf88e0b378237f89387094b20e
wdvxdr <wdvxdr1123@gmail.com>
2021-10-07 21:40:20 +0800

    feat(server): use nhooyr.io/websocket

--- go.mod
+++ go.mod
@@ -11,7 +11,6 @@ require (
 	github.com/dustin/go-humanize v1.0.0
 	github.com/fumiama/go-hide-param v0.1.4
 	github.com/gabriel-vasile/mimetype v1.3.1
-	github.com/gorilla/websocket v1.4.2
 	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/pkg/errors v0.9.1
@@ -26,13 +25,16 @@ require (
 	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+	nhooyr.io/websocket v1.8.7
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	github.com/google/uuid v1.1.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/klauspost/compress v1.10.3 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.6 // indirect
 	github.com/lestrrat-go/strftime v1.0.5 // indirect
 	github.com/maruel/rs v0.0.0-20150922171536-2c81c4312fe4 // indirect
@@ -48,7 +50,6 @@ require (
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
 	modernc.org/libc v1.8.1 // indirect
 	modernc.org/mathutil v1.2.2 // indirect
 	modernc.org/memory v1.0.4 // indirect
--- server/websocket.go
+++ server/websocket.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -11,13 +13,13 @@ import (
 	"time"
 
 	"gopkg.in/yaml.v3"
+	"nhooyr.io/websocket"
 
 	"github.com/Mrs4s/go-cqhttp/coolq"
 	"github.com/Mrs4s/go-cqhttp/global"
 	"github.com/Mrs4s/go-cqhttp/modules/config"
 
 	"github.com/Mrs4s/MiraiGo/utils"
-	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
 	"github.com/tidwall/gjson"
 )
@@ -46,14 +48,7 @@ type websocketClient struct {
 
 type webSocketConn struct {
 	*websocket.Conn
-	sync.Mutex
 	apiCaller *apiCaller
-}
-
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
 }
 
 // runWSServer 运行一个正向WS server
@@ -129,7 +124,7 @@ func (c *websocketClient) connectAPI() {
 	if c.token != "" {
 		header["Authorization"] = []string{"Token " + c.token}
 	}
-	conn, _, err := websocket.DefaultDialer.Dial(c.conf.API, header) // nolint
+	conn, _, err := websocket.Dial(context.Background(), c.conf.Universal, &websocket.DialOptions{HTTPHeader: header}) // nolint
 	if err != nil {
 		log.Warnf("连接到反向WebSocket API服务器 %v 时出现错误: %v", c.conf.API, err)
 		if c.conf.ReconnectInterval != 0 {
@@ -156,7 +151,7 @@ func (c *websocketClient) connectEvent() {
 	if c.token != "" {
 		header["Authorization"] = []string{"Token " + c.token}
 	}
-	conn, _, err := websocket.DefaultDialer.Dial(c.conf.Event, header) // nolint
+	conn, _, err := websocket.Dial(context.Background(), c.conf.Universal, &websocket.DialOptions{HTTPHeader: header}) // nolint
 	if err != nil {
 		log.Warnf("连接到反向WebSocket Event服务器 %v 时出现错误: %v", c.conf.Event, err)
 		if c.conf.ReconnectInterval != 0 {
@@ -168,7 +163,7 @@ func (c *websocketClient) connectEvent() {
 
 	handshake := fmt.Sprintf(`{"meta_event_type":"lifecycle","post_type":"meta_event","self_id":%d,"sub_type":"connect","time":%d}`,
 		c.bot.Client.Uin, time.Now().Unix())
-	err = conn.WriteMessage(websocket.TextMessage, []byte(handshake))
+	err = conn.Write(context.Background(), websocket.MessageText, []byte(handshake))
 	if err != nil {
 		log.Warnf("反向WebSocket 握手时出现错误: %v", err)
 	}
@@ -192,7 +187,7 @@ func (c *websocketClient) connectUniversal() {
 	if c.token != "" {
 		header["Authorization"] = []string{"Token " + c.token}
 	}
-	conn, _, err := websocket.DefaultDialer.Dial(c.conf.Universal, header) // nolint
+	conn, _, err := websocket.Dial(context.Background(), c.conf.Universal, &websocket.DialOptions{HTTPHeader: header}) // nolint
 	if err != nil {
 		log.Warnf("连接到反向WebSocket Universal服务器 %v 时出现错误: %v", c.conf.Universal, err)
 		if c.conf.ReconnectInterval != 0 {
@@ -203,7 +198,7 @@ func (c *websocketClient) connectUniversal() {
 	}
 	handshake := fmt.Sprintf(`{"meta_event_type":"lifecycle","post_type":"meta_event","self_id":%d,"sub_type":"connect","time":%d}`,
 		c.bot.Client.Uin, time.Now().Unix())
-	err = conn.WriteMessage(websocket.TextMessage, []byte(handshake))
+	err = conn.Write(context.Background(), websocket.MessageText, []byte(handshake))
 	if err != nil {
 		log.Warnf("反向WebSocket 握手时出现错误: %v", err)
 	}
@@ -221,10 +216,10 @@ func (c *websocketClient) connectUniversal() {
 }
 
 func (c *websocketClient) listenAPI(conn *webSocketConn, u bool) {
-	defer func() { _ = conn.Close() }()
+	defer func() { _ = conn.Close(websocket.StatusNormalClosure, "") }()
 	for {
 		buffer := global.NewBuffer()
-		t, reader, err := conn.NextReader()
+		t, reader, err := conn.Conn.Reader(context.Background())
 		if err != nil {
 			log.Warnf("监听反向WS API时出现错误: %v", err)
 			break
@@ -234,7 +229,7 @@ func (c *websocketClient) listenAPI(conn *webSocketConn, u bool) {
 			log.Warnf("监听反向WS API时出现错误: %v", err)
 			break
 		}
-		if t == websocket.TextMessage {
+		if t == websocket.MessageText {
 			go func(buffer *bytes.Buffer) {
 				defer global.PutBuffer(buffer)
 				conn.handleRequest(c.bot, buffer.Bytes())
@@ -258,13 +253,12 @@ func (c *websocketClient) onBotPushEvent(e *coolq.Event) {
 		return
 	}
 	push := func(conn *webSocketConn, reconnect func()) {
-		log.Debugf("向WS服务器 %v 推送Event: %s", conn.RemoteAddr().String(), e.JSONBytes())
-		conn.Lock()
-		defer conn.Unlock()
-		_ = conn.SetWriteDeadline(time.Now().Add(time.Second * 15))
-		if err := conn.WriteMessage(websocket.TextMessage, e.JSONBytes()); err != nil {
-			log.Warnf("向WS服务器 %v 推送Event时出现错误: %v", conn.RemoteAddr().String(), err)
-			_ = conn.Close()
+		log.Debugf("向WS服务器推送Event: %s", e.JSONBytes())
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+		defer cancel()
+		if err := conn.Write(ctx, websocket.MessageText, e.JSONBytes()); err != nil {
+			log.Warnf("向WS服务器推送 Event 时出现错误: %v", err)
+			_ = conn.Close(websocket.StatusNormalClosure, "")
 			if c.conf.ReconnectInterval != 0 {
 				time.Sleep(time.Millisecond * time.Duration(c.conf.ReconnectInterval))
 				reconnect()
@@ -286,15 +280,15 @@ func (s *webSocketServer) event(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(status)
 		return
 	}
-	c, err := upgrader.Upgrade(w, r, nil)
+	c, err := websocket.Accept(w, r, nil)
 	if err != nil {
 		log.Warnf("处理 WebSocket 请求时出现错误: %v", err)
 		return
 	}
-	err = c.WriteMessage(websocket.TextMessage, []byte(s.handshake))
+	err = c.Write(context.Background(), websocket.MessageText, []byte(s.handshake))
 	if err != nil {
 		log.Warnf("WebSocket 握手时出现错误: %v", err)
-		_ = c.Close()
+		_ = c.Close(websocket.StatusNormalClosure, "")
 		return
 	}
 
@@ -314,7 +308,7 @@ func (s *webSocketServer) api(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(status)
 		return
 	}
-	c, err := upgrader.Upgrade(w, r, nil)
+	c, err := websocket.Accept(w, r, nil)
 	if err != nil {
 		log.Warnf("处理 WebSocket 请求时出现错误: %v", err)
 		return
@@ -334,15 +328,15 @@ func (s *webSocketServer) any(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(status)
 		return
 	}
-	c, err := upgrader.Upgrade(w, r, nil)
+	c, err := websocket.Accept(w, r, nil)
 	if err != nil {
 		log.Warnf("处理 WebSocket 请求时出现错误: %v", err)
 		return
 	}
-	err = c.WriteMessage(websocket.TextMessage, []byte(s.handshake))
+	err = c.Write(context.Background(), websocket.MessageText, []byte(s.handshake))
 	if err != nil {
 		log.Warnf("WebSocket 握手时出现错误: %v", err)
-		_ = c.Close()
+		_ = c.Close(websocket.StatusNormalClosure, "")
 		return
 	}
 	log.Infof("接受 WebSocket 连接: %v (/)", r.RemoteAddr)
@@ -357,10 +351,10 @@ func (s *webSocketServer) any(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *webSocketServer) listenAPI(c *webSocketConn) {
-	defer func() { _ = c.Close() }()
+	defer func() { _ = c.Close(websocket.StatusNormalClosure, "") }()
 	for {
 		buffer := global.NewBuffer()
-		t, reader, err := c.NextReader()
+		t, reader, err := c.Reader(context.Background())
 		if err != nil {
 			break
 		}
@@ -369,7 +363,7 @@ func (s *webSocketServer) listenAPI(c *webSocketConn) {
 			break
 		}
 
-		if t == websocket.TextMessage {
+		if t == websocket.MessageText {
 			go func(buffer *bytes.Buffer) {
 				defer global.PutBuffer(buffer)
 				c.handleRequest(s.bot, buffer.Bytes())
@@ -384,7 +378,7 @@ func (c *webSocketConn) handleRequest(_ *coolq.CQBot, payload []byte) {
 	defer func() {
 		if err := recover(); err != nil {
 			log.Printf("处置WS命令时发生无法恢复的异常：%v\n%s", err, debug.Stack())
-			_ = c.Close()
+			_ = c.Close(websocket.StatusInternalError, fmt.Sprint(err))
 		}
 	}()
 	j := gjson.Parse(utils.B2S(payload))
@@ -394,9 +388,9 @@ func (c *webSocketConn) handleRequest(_ *coolq.CQBot, payload []byte) {
 	if j.Get("echo").Exists() {
 		ret["echo"] = j.Get("echo").Value()
 	}
-	c.Lock()
-	defer c.Unlock()
-	_ = c.WriteJSON(ret)
+	writer, _ := c.Writer(context.Background(), websocket.MessageText)
+	_ = json.NewEncoder(writer).Encode(ret)
+	_ = writer.Close()
 }
 
 func (s *webSocketServer) onBotPushEvent(e *coolq.Event) {
@@ -411,10 +405,9 @@ func (s *webSocketServer) onBotPushEvent(e *coolq.Event) {
 	j := 0
 	for i := 0; i < len(s.eventConn); i++ {
 		conn := s.eventConn[i]
-		log.Debugf("向WS客户端 %v 推送Event: %s", conn.RemoteAddr().String(), e.JSONBytes())
-		conn.Lock()
-		if err := conn.WriteMessage(websocket.TextMessage, e.JSONBytes()); err != nil {
-			_ = conn.Close()
+		log.Debugf("向WS客户端推送Event: %s", e.JSONBytes())
+		if err := conn.Write(context.Background(), websocket.MessageText, e.JSONBytes()); err != nil {
+			_ = conn.Close(websocket.StatusNormalClosure, "")
 			conn = nil
 			continue
 		}
@@ -423,7 +416,6 @@ func (s *webSocketServer) onBotPushEvent(e *coolq.Event) {
 			// use a in-place removal to avoid copying.
 			s.eventConn[j] = conn
 		}
-		conn.Unlock()
 		j++
 	}
 	s.eventConn = s.eventConn[:j]
