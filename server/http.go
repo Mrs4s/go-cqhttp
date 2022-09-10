cf3667dbf2c98294808ef6a8c4e2102a4c5b0fa9
jellycreator <577430840@qq.com>
2022-09-10 18:22:03 +0800

    修复了yaml生成的格式错误

--- server/http.go
+++ server/http.go
@@ -80,22 +80,22 @@ type httpCtx struct {
 
 const httpDefault = `
   - http: # HTTP 通信设置
-      address: 0.0.0.0:5700 # HTTP监听地址
-      timeout: 5      # 反向 HTTP 超时时间, 单位秒，<5 时将被忽略
-      long-polling:   # 长轮询拓展
-        enabled: false       # 是否开启
-        max-queue-size: 2000 # 消息队列大小，0 表示不限制队列大小，谨慎使用
-      middlewares:
-        <<: *default # 引用默认中间件
-      post:           # 反向HTTP POST地址列表
-      #- url: ''                # 地址
-      #  secret: ''             # 密钥
-      #  max-retries: 3         # 最大重试，0 时禁用
-      #  retries-interval: 1500 # 重试时间，单位毫秒，0 时立即
-      #- url: http://127.0.0.1:5701/ # 地址
-      #  secret: ''                  # 密钥
-      #  max-retries: 10             # 最大重试，0 时禁用
-      #  retries-interval: 1000      # 重试时间，单位毫秒，0 时立即
+	address: 0.0.0.0:5700 # HTTP监听地址
+	timeout: 5      # 反向 HTTP 超时时间, 单位秒，<5 时将被忽略
+	long-polling:   # 长轮询拓展
+	enabled: false       # 是否开启
+	max-queue-size: 2000 # 消息队列大小，0 表示不限制队列大小，谨慎使用
+	middlewares:
+	<<: *default # 引用默认中间件
+	post:           # 反向HTTP POST地址列表
+	#- url: ''                # 地址
+	#  secret: ''             # 密钥
+	#  max-retries: 3         # 最大重试，0 时禁用
+	#  retries-interval: 1500 # 重试时间，单位毫秒，0 时立即
+	#- url: http://127.0.0.1:5701/ # 地址
+	#  secret: ''                  # 密钥
+	#  max-retries: 10             # 最大重试，0 时禁用
+	#  retries-interval: 1000      # 重试时间，单位毫秒，0 时立即
 `
 
 func init() {
