57f2030d4e9d0e4d60a5367fcf3955a7c56cc66e
zhullyb <zhullyb@outlook.com>
2023-11-14 11:07:27 +0800

    fix: switch ghproxy.com to mirror.ghproxy.com
    ghproxy.com 已经被墙，根据其网页上的通知应当更换为二级域名 mirror.ghproxy.com，或考虑使用别的反代服务

--- cmd/gocq/main.go
+++ cmd/gocq/main.go
@@ -483,7 +483,7 @@ func getRemoteLatestProtocolVersion(protocolType int) ([]byte, error) {
 	}
 	response, err := download.Request{URL: url}.Bytes()
 	if err != nil {
-		return download.Request{URL: "https://ghproxy.com/" + url}.Bytes()
+		return download.Request{URL: "https://mirror.ghproxy.com/" + url}.Bytes()
 	}
 	return response, nil
 }
